8e4b83028c8a1acc0830dfe9a4fb7aaef10d46ca
unknown <thiagodevprofissional@gmail.com>
2024-10-10 12:41:34 -0300

    adicionado index.html

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// Muda a cor do header ao scrollar
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
